2e28cacc2cc6d373886217d0f442c871e9011e17
Kae Okamura <kaehehehe@gmail.com>
2021-05-08 08:02:39 +0900

    Add play and pause feature

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ const musicImg = document.querySelector('.music-image img');
 const musicTitle = document.querySelector('.title');
 const musicArtist = document.querySelector('.artist');
 const musicAudio = document.querySelector('audio');
+const playBtn = document.querySelector('#play');
 
 let musicIndex = 3;
 
@@ -18,3 +19,20 @@ function loadMusic(musicIndex) {
   musicImg.src = `/image/${musicList[musicIndex].image}.jpg`;
   musicAudio.src = `/audio/${musicList[musicIndex].audio}.mp3`;
 }
+
+function playMusic() {
+  musicAudio.classList.add('paused');
+  playBtn.innerText = 'pause';
+  musicAudio.play();
+}
+
+function pauseMusic() {
+  musicAudio.classList.remove('paused');
+  playBtn.innerText = 'play_arrow';
+  musicAudio.pause();
+}
+
+playBtn.addEventListener('click', () => {
+  const isMusicPaused = musicAudio.classList.contains('paused');
+  isMusicPaused ? pauseMusic(): playMusic() ;
+});
