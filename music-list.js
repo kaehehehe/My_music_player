8d629c26149bd5a2f3d26c8a8f34d27b3f595556
Kae Okamura <kaehehehe@gmail.com>
2021-05-08 00:17:08 +0900

    Add Music list

---
+++ music-list.js
@@ -0,0 +1,26 @@
+const musicList = [
+  {
+    name: "Life",
+    artist: "DJ Quads",
+    image: "image1",
+    audio: "music1"
+  },
+  {
+    name: "Warm Nights",
+    artist: "LAKEY INSPIRED",
+    image: "2",
+    audio: "music2"
+  },
+  {
+    name: "Ice Tea",
+    artist: "Not The King",
+    image: "3",
+    audio: "music3"
+  },
+  {
+    name: "When You Move Your Body",
+    artist: "Quesa",
+    image: "4",
+    audio: "music4"
+  }
+]
