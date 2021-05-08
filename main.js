7aa15b77c6076edaea29d97356c39778cf11581f
Kae Okamura <kaehehehe@gmail.com>
2021-05-08 09:40:05 +0900

    Add next and prev feature

--- main.js
+++ main.js
@@ -6,18 +6,20 @@ const musicTitle = document.querySelector('.title');
 const musicArtist = document.querySelector('.artist');
 const musicAudio = document.querySelector('audio');
 const playBtn = document.querySelector('#play');
+const prevBtn = document.querySelector('#prev');
+const nextBtn = document.querySelector('#next');
 
-let musicIndex = 3;
+let musicNum = 1;
 
 window.addEventListener('load', () => {
-  loadMusic(musicIndex);
+  loadMusic(musicNum);
 });
 
-function loadMusic(musicIndex) {
-  musicTitle.innerText = musicList[musicIndex].name;
-  musicArtist.innerText = musicList[musicIndex].artist;
-  musicImg.src = `/image/${musicList[musicIndex].image}.jpg`;
-  musicAudio.src = `/audio/${musicList[musicIndex].audio}.mp3`;
+function loadMusic(index) {
+  musicTitle.innerText = musicList[index -1].name;
+  musicArtist.innerText = musicList[index -1].artist;
+  musicImg.src = `/image/${musicList[index -1].image}.jpg`;
+  musicAudio.src = `/audio/${musicList[index -1].audio}.mp3`;
 }
 
 function playMusic() {
@@ -32,7 +34,30 @@ function pauseMusic() {
   musicAudio.pause();
 }
 
+function nextMusic() {
+  musicNum ++;
+  musicNum > musicList.length ? musicNum = 1 : musicNum = musicNum;
+  loadMusic(musicNum);
+  playMusic();
+  
+}
+
+function prevMusic() {
+  musicNum --;
+  musicNum < 1 ? musicNum = musicList.length : musicNum = musicNum;
+  loadMusic(musicNum);
+  playMusic();
+}
+
 playBtn.addEventListener('click', () => {
   const isMusicPaused = musicAudio.classList.contains('paused');
   isMusicPaused ? pauseMusic(): playMusic() ;
+});
+
+nextBtn.addEventListener('click', () => {
+  nextMusic();
+});
+
+prevBtn.addEventListener('click', () => {
+  prevMusic();
 });
